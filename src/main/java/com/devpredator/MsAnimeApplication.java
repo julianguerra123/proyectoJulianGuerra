bc0d6c5e73f59139fd20fbb87b9b061ab4e682f4
julianguerra123 <131739359+julianguerra123@users.noreply.github.com>
2024-05-20 22:22:00 -0500

    Add files via upload

---
+++ src/main/java/com/devpredator/MsAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.devpredator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpredator/MsAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpredator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
